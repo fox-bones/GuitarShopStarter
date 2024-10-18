d24514ce43960d162722cab142cf5d61ed2f31d3
fox-bones <mrcaysizzle@gmail.com>
2024-10-17 21:25:13 -0400

    first

---
+++ GuitarShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("/ContactUs")]
+        public IActionResult ContactUs()
+        {
+            var contact = new Dictionary<string, string>
+            {
+                { "Phone: ", "555-123-4567" },
+                { "Email: ", "info@myguitarshop.com" },
+                { "Cell: ", "555-234-5678" }
+            };
+            return View(contact);
+        }
+    }
+}
